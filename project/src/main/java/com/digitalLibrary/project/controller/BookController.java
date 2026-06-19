e60505aa8f32bae08b43fbfcc55ac050f5d96d04
omkar-aher04 <aheromkar04@gmail.com>
2026-06-19 15:08:12 +0530

    removed redis for free deployment

--- project/src/main/java/com/digitalLibrary/project/controller/BookController.java
+++ project/src/main/java/com/digitalLibrary/project/controller/BookController.java
@@ -2,7 +2,6 @@ package com.digitalLibrary.project.controller;
 
 import com.digitalLibrary.project.entity.BookEntity;
 import com.digitalLibrary.project.service.BookService;
-import com.digitalLibrary.project.service.RedisBooksService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,27 +10,25 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
-    private final RedisBooksService redisBooksService;
 
-    public BookController (BookService bookService, BookService bookService1, RedisBooksService redisBooksService){
-        this.bookService = bookService1;
-        this.redisBooksService = redisBooksService;
+    public BookController ( BookService bookService){
+        this.bookService = bookService;
 
     }
     //add books
     @PostMapping("/addbooks")
     public BookEntity addBook(@RequestBody BookEntity bookEntity){
-        return redisBooksService.addBook(bookEntity);
+        return bookService.addBook(bookEntity);
     }
     //get books
     @GetMapping("/allbooks")
     public List<BookEntity> getAllBooks(){
-        return redisBooksService.getAllBooks();
+        return bookService.getAllBooks();
     }
     // delete books
     @DeleteMapping("/{id}")
     public String deleteById(@PathVariable Integer id){
-       return redisBooksService.deleteById(id);
+       return bookService.deleteById(id);
     }
     // update book
 }
